14568cd3b5d750fed5a9856128ad603f525088f8
Daniel <danielhernandez4352@gmail.com>
2022-02-03 21:57:30 +0100

    prueba sobre insertData

---
+++ src/main/resources/insertCompanies.sql
@@ -0,0 +1,200 @@
+insert into companies (nombre, nacionalidad)
+values ('Stehr Inc', 'Slovenia');
+insert into companies (nombre, nacionalidad)
+values ('Langosh LLC', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Parker, Ankunding and Wuckert', 'Tanzania');
+insert into companies (nombre, nacionalidad)
+values ('Schaefer and Sons', 'Argentina');
+insert into companies (nombre, nacionalidad)
+values ('Dietrich, Towne and Zemlak', 'Costa Rica');
+insert into companies (nombre, nacionalidad)
+values ('Macejkovic-Sawayn', 'Mexico');
+insert into companies (nombre, nacionalidad)
+values ('Walter-Funk', 'France');
+insert into companies (nombre, nacionalidad)
+values ('Schroeder Group', 'Japan');
+insert into companies (nombre, nacionalidad)
+values ('Shields-Johns', 'Indonesia');
+insert into companies (nombre, nacionalidad)
+values ('Ullrich-Kemmer', 'Indonesia');
+insert into companies (nombre, nacionalidad)
+values ('Rice-Kassulke', 'Vietnam');
+insert into companies (nombre, nacionalidad)
+values ('Dickinson-Bogisich', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Gaylord-Pacocha', 'United Kingdom');
+insert into companies (nombre, nacionalidad)
+values ('Gibson, Brakus and Wilderman', 'France');
+insert into companies (nombre, nacionalidad)
+values ('Stroman LLC', 'Peru');
+insert into companies (nombre, nacionalidad)
+values ('Moen-Lowe', 'Norway');
+insert into companies (nombre, nacionalidad)
+values ('Gleichner-Reinger', 'Belarus');
+insert into companies (nombre, nacionalidad)
+values ('Gottlieb and Sons', 'Vietnam');
+insert into companies (nombre, nacionalidad)
+values ('Schmeler Inc', 'Iran');
+insert into companies (nombre, nacionalidad)
+values ('Connelly-Moore', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Crona Inc', 'Sweden');
+insert into companies (nombre, nacionalidad)
+values ('Murray-Keebler', 'Mexico');
+insert into companies (nombre, nacionalidad)
+values ('Jones-Farrell', 'Philippines');
+insert into companies (nombre, nacionalidad)
+values ('Maggio-Breitenberg', 'Ukraine');
+insert into companies (nombre, nacionalidad)
+values ('Casper-Fisher', 'Chile');
+insert into companies (nombre, nacionalidad)
+values ('Douglas-Balistreri', 'Armenia');
+insert into companies (nombre, nacionalidad)
+values ('Blick-Sawayn', 'Namibia');
+insert into companies (nombre, nacionalidad)
+values ('Corkery-Kshlerin', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Upton and Sons', 'Czech Republic');
+insert into companies (nombre, nacionalidad)
+values ('Jaskolski, Bradtke and Waelchi', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Kuvalis, Haag and McCullough', 'France');
+insert into companies (nombre, nacionalidad)
+values ('Strosin, Kuhn and Maggio', 'Philippines');
+insert into companies (nombre, nacionalidad)
+values ('Sawayn, Littel and Maggio', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Towne LLC', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Veum, Hills and Stehr', 'Indonesia');
+insert into companies (nombre, nacionalidad)
+values ('Gusikowski Inc', 'Greece');
+insert into companies (nombre, nacionalidad)
+values ('Upton, Halvorson and Conroy', 'Pakistan');
+insert into companies (nombre, nacionalidad)
+values ('Willms Inc', 'Democratic Republic of the Congo');
+insert into companies (nombre, nacionalidad)
+values ('Stamm, Boehm and Brown', 'Kuwait');
+insert into companies (nombre, nacionalidad)
+values ('Abernathy-Deckow', 'Colombia');
+insert into companies (nombre, nacionalidad)
+values ('Williamson, Reichel and Bergstrom', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Shields, Cartwright and Pagac', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Weissnat and Sons', 'South Korea');
+insert into companies (nombre, nacionalidad)
+values ('Weimann, Dach and Torp', 'Indonesia');
+insert into companies (nombre, nacionalidad)
+values ('Upton Group', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Keebler Group', 'Brazil');
+insert into companies (nombre, nacionalidad)
+values ('Kuvalis Inc', 'Thailand');
+insert into companies (nombre, nacionalidad)
+values ('Kilback LLC', 'Sweden');
+insert into companies (nombre, nacionalidad)
+values ('Reinger, O''Connell and Christiansen', 'Madagascar');
+insert into companies (nombre, nacionalidad)
+values ('Nikolaus-Towne', 'Mauritius');
+insert into companies (nombre, nacionalidad)
+values ('Harvey-Stroman', 'Nigeria');
+insert into companies (nombre, nacionalidad)
+values ('Jaskolski, Parker and Wolff', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Klein, Volkman and Brown', 'Sweden');
+insert into companies (nombre, nacionalidad)
+values ('Kertzmann, Paucek and Crona', 'Brazil');
+insert into companies (nombre, nacionalidad)
+values ('Torphy, Blanda and Boehm', 'Tunisia');
+insert into companies (nombre, nacionalidad)
+values ('Barrows, Reilly and Blick', 'Indonesia');
+insert into companies (nombre, nacionalidad)
+values ('Smitham, Stroman and Dickens', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Denesik Group', 'France');
+insert into companies (nombre, nacionalidad)
+values ('Wintheiser, Bergstrom and Rosenbaum', 'Moldova');
+insert into companies (nombre, nacionalidad)
+values ('Bruen Inc', 'Russia');
+insert into companies (nombre, nacionalidad)
+values ('Watsica-Lemke', 'Russia');
+insert into companies (nombre, nacionalidad)
+values ('Schulist-Bechtelar', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Johnson, Reinger and Abernathy', 'Germany');
+insert into companies (nombre, nacionalidad)
+values ('Schimmel Inc', 'Colombia');
+insert into companies (nombre, nacionalidad)
+values ('Veum-Tillman', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Maggio, Cruickshank and Weber', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Zulauf, Barrows and Dare', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Deckow-Rodriguez', 'Zimbabwe');
+insert into companies (nombre, nacionalidad)
+values ('Spinka LLC', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Bahringer LLC', 'France');
+insert into companies (nombre, nacionalidad)
+values ('Gibson Group', 'Indonesia');
+insert into companies (nombre, nacionalidad)
+values ('Hilll and Sons', 'Peru');
+insert into companies (nombre, nacionalidad)
+values ('Upton, Kilback and Labadie', 'Russia');
+insert into companies (nombre, nacionalidad)
+values ('Hane-Bosco', 'Philippines');
+insert into companies (nombre, nacionalidad)
+values ('DuBuque Group', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Collins, Emmerich and Parisian', 'Philippines');
+insert into companies (nombre, nacionalidad)
+values ('Macejkovic-Mills', 'Argentina');
+insert into companies (nombre, nacionalidad)
+values ('Kuhn, Jacobson and Schmitt', 'Bolivia');
+insert into companies (nombre, nacionalidad)
+values ('Runolfsdottir, Prosacco and Stark', 'Yemen');
+insert into companies (nombre, nacionalidad)
+values ('Larkin, Hansen and Schaden', 'Japan');
+insert into companies (nombre, nacionalidad)
+values ('Lindgren, Howell and Runolfsson', 'Peru');
+insert into companies (nombre, nacionalidad)
+values ('Dibbert-Grant', 'Australia');
+insert into companies (nombre, nacionalidad)
+values ('Conn, Kling and Rogahn', 'Portugal');
+insert into companies (nombre, nacionalidad)
+values ('Altenwerth LLC', 'Cuba');
+insert into companies (nombre, nacionalidad)
+values ('Spencer Group', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Rippin-Goyette', 'Cuba');
+insert into companies (nombre, nacionalidad)
+values ('Gerlach, Terry and Cronin', 'France');
+insert into companies (nombre, nacionalidad)
+values ('Shields LLC', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Considine and Sons', 'Brazil');
+insert into companies (nombre, nacionalidad)
+values ('Hoeger, Collier and Hessel', 'Philippines');
+insert into companies (nombre, nacionalidad)
+values ('Connelly-Nikolaus', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Cummerata, Hammes and Goyette', 'China');
+insert into companies (nombre, nacionalidad)
+values ('Hilpert Group', 'Russia');
+insert into companies (nombre, nacionalidad)
+values ('Batz, Moen and Robel', 'Moldova');
+insert into companies (nombre, nacionalidad)
+values ('Crona and Sons', 'Latvia');
+insert into companies (nombre, nacionalidad)
+values ('Koss, Kautzer and Bogisich', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Purdy Group', 'Haiti');
+insert into companies (nombre, nacionalidad)
+values ('Wolff Inc', 'Poland');
+insert into companies (nombre, nacionalidad)
+values ('Wiegand, Lehner and Hackett', 'Russia');
+insert into companies (nombre, nacionalidad)
+values ('Kihn-Dickinson', 'Madagascar');
