b75cdb1b86c801a7156ab37985410c33e3f628fb
Daniel <danielhernandez4352@gmail.com>
2022-02-03 21:45:55 +0100

    Setup 2.0

---
+++ src/main/java/repository/CompaniesRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import entity.Companies;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompaniesRepository extends JpaRepository<Companies, Integer> {
+}
